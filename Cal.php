25bfbf614a487abed8e5d0d3fcaa0406e2ff5697
namnguyen <toilanam97@gmail.com>
2019-09-06 13:49:36 +0700

    Check validate on DIVISION

---
+++ Cal.php
@@ -0,0 +1,33 @@
+<?php
+//
+//
+//class Cal
+//{
+//public $first;
+//public $second;
+//public function __construct($first,$second)
+//{
+//    $this->second =$second;
+//    $this->first = $first;
+//}
+//    public function getFirst()
+//    {
+//        return $this->first;
+//    }
+//    public function getSecond()
+//    {
+//        return $this->second;
+//    }
+//public function cong(){
+//    echo $this->first+ $this->second;
+//}
+//public function tru(){
+//    return $this->first + $this->second;
+//}
+//public function nhan(){
+//    return $this->first* $this->second;
+//}
+//public function chia(){
+//    return $this->first/$this->second;
+//}
+//}
